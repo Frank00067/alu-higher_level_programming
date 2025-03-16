12da30e1d582b662acb35d4ba96a54bc9985abf5
Frank00067 <f.nkurunziz@alustudent.com>
2025-03-16 06:38:52 -0700

    ReadMe

--- python-network_0/3-methods.sh
+++ python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send a GET request to a given URL with a header variable.
-curl -sH "X-School-User-Id: 98" "$1"
+# Display all HTTP methods the server of a given URL will accept.
+curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
